52f8bf091057317d00c0425339b71eb00b7af8b5
M Akif Eyler <ma.eyler@gmail.com>
2018-07-21 14:28:13 +0300

    Inspector V1.3
    * click on an Iterable makes an Array
    * Set methods are added

--- sss/inspector.js
+++ sss/inspector.js
@@ -1,5 +1,5 @@
 "use strict";
-const VERSION = "V1.2";
+const VERSION = "V1.3", ITERABLE = new Object();
 const objA = [], objP = [], prop = [], meth = [];
 var hist = [];    //object history -- global variables
 var current = 0;  //current object index in list1 & objA
@@ -20,29 +20,26 @@ function doMethod(met) {
     if (!ff) ff = co.__proto__[met];
     if (typeof ff != "function") return;
     let n = ff.length;
-    let s = "Enter "+n+" arguments separated by commas ";
+    let s = "Enter ";
+    if (n == 0) s += "optional arguments "; 
+    if (n == 1) s += "the argument ";
+    if (n >= 2) s += n+" arguments separated by commas ";
     let arg = "("; while (--n > 0) arg += ", "; arg += ")";
     arg = prompt(s+"in order to call "+met+"()", arg);
     if (arg) try {
-        if (arg[0] != "(" || !arg.endsWith(")")) 
-            throw (Error(arg));
         let cmd = "co."+met+arg;
-        report(cmd, eval(cmd));
+        if (arg[0] != "(" || !arg.endsWith(")")) 
+             reportError(Error(cmd));
+        else report(cmd, eval(cmd));
     } catch(e) {
         reportError(e);
     }
 }
-function doClick(t) { //t is target, <ul> element
-    let y = t.scrollTop + event.layerY;
-    let h = t.firstChild.clientHeight + 2; //16px
-    let k = Math.trunc(y/h);
-    if (t == list1) {
-        displayItem(k);
-    } else if (t == list2) {
-        display(objP[k]);
-    } else if (t == list3) {
-        doMethod(meth[k]);
-    }
+function doClick2() { //target == list2
+    let k = lineNumber(list2), obj = objP[k];
+    if (obj === ITERABLE) 
+         display(objToArray(co));
+    else display(obj);
 }
 function previous() {
     let n = hist.length; if (n<2) return;
@@ -77,20 +74,20 @@ function doEnter() {
         reportError(e);
     }
 }
-function doInput() {
-    console.log(event);
-}
 function trunc(s, M) { //if s is long, truncate to M chars
     if (s == null) s = "null";
     if (s.length > M+5) s = s.substring(0, M)+"...";
     return s;
+}
+function objToArray(obj) {
+    let a = []; for (let x of obj) a.push(x); return a;
 }
 function objToString(obj) {
     const LT = /</g;
     let s = obj.toString().replace(LT, "&lt;");
     if (typeof obj == "string") s = '"'+s+'"';
     else if (obj instanceof Array) s = '['+s+']';
-    //else if (obj instanceof Set) s = '{'+s+'}';
+    else if (obj instanceof Set) s = '{'+objToArray(obj)+'}';
     return s;
 }
 function arrayToList(a, L) {
@@ -116,13 +113,10 @@ function display(f) {
     if (!f) return; let t = typeof f;
     if (t != "string" && t != "object") return;
     if (f instanceof Promise) { //nothing to display in f
-        f.then(display, reportError);
-        out.innerText = "A Promise was made - takes time";
+        f.then(display, reportError); console.log(f);
+        out.innerText = "A Promise was made";
         out.style.backgroundColor = "cyan"; return;
     }
-    /*if (f instanceof Symbol.iterator) {
-        a = []; for (let x of f) a.push(x); f = a;
-    }*/
     let i = objA.indexOf(f);
     if (i >= 0) {
         displayItem(i);
@@ -153,18 +147,24 @@ function addMethods(metA) {
 }
 function add_proto_() {
     if (typeof co == "string") 
-  addMethods(["charAt", "concat", "replace", "search", "split", "substring"]);
+addMethods(["charAt", "concat", "replace", "search", "split", "substring"]);
     if (co instanceof Array) 
         addMethods(["indexOf", "join", "push", "pop", "reverse", "splice"]);
+    if (co instanceof Set) 
+        addMethods(["add", "clear", "delete", "has", "values"]);
     if (co instanceof RegExp) { 
         addMethods(["test", "exec"]); addProperty("flags");
     }
+    /*if (co instanceof Error) { 
+        addProperty("fileName"); addProperty("stack");
+    }*/
 }
 function displayItem(c) {
     //co is global -- current object
     selectItem(c); co = objA[current];
     objP.length = 1; prop.length = 1; meth.length = 0;
-    objP[0] = undefined; prop[0] = "* *"+co.constructor.name;
+    objP[0] = ITERABLE; prop[0] = "* *"+co.constructor.name;
+    if (co[Symbol.iterator]) prop[0] += " — Iterable"
     for (let key in co) 
       try {
         if (key == "webkitStorageInfo") continue;
@@ -181,7 +181,7 @@ function displayItem(c) {
     add_proto_(); list1.focus();
     arrayToList(prop, list2);
     arrayToList(meth, list3);
-    let p = prop.length - 1, m = meth.length;
+    let p = prop.length-1, m = meth.length;
     let s = p+" properties and "+m+" methods";
     out.innerText = trunc(co.toString(), 25)+" — "+s; 
     out.style.backgroundColor = "";
