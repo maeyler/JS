ce3d962c6d15ee0ec8d6a823591a9f47c9dc66d6
M Akif Eyler <ma.eyler@gmail.com>
2018-07-23 00:16:16 +0300

    Inspector V1.4
    * length for Array & String
    * size for Set &Map
    * Tooltips are hidden in 2.5 sec
    * current object is "_"

--- sss/inspector.js
+++ sss/inspector.js
@@ -1,9 +1,9 @@
 "use strict";
-const VERSION = "V1.3", ITERABLE = new Object();
-const objA = [], objP = [], prop = [], meth = [];
+const VERSION = "V1.4", ITERABLE = new Object();
+const objA = [], objP = [];
 var hist = [];    //object history -- global variables
 var current = 0;  //current object index in list1 & objA
-var co = 0;  //current object
+var _ = 0;  //current object
 function report(input, result) { 
     display(result); 
     let msg = trunc(input, 25)+trunc(" ⇒ "+result, 40); 
@@ -14,10 +14,11 @@ function reportError(e) {
     console.log(e); out.innerText = trunc(" "+e, 60); 
     out.style.backgroundColor = "pink";
 }
-function doMethod(met) {
-    //co = objA[current];
-    let ff = co[met]; //simpler than reflection
-    if (!ff) ff = co.__proto__[met];
+function doMethod(c) {
+    let met = list3.children[c].innerText;
+    //_ = objA[current];
+    let ff = _[met]; //simpler than reflection
+    if (!ff) ff = _.__proto__[met];
     if (typeof ff != "function") return;
     let n = ff.length;
     let s = "Enter ";
@@ -27,7 +28,7 @@ function doMethod(met) {
     let arg = "("; while (--n > 0) arg += ", "; arg += ")";
     arg = prompt(s+"in order to call "+met+"()", arg);
     if (arg) try {
-        let cmd = "co."+met+arg;
+        let cmd = "_."+met+arg;
         if (arg[0] != "(" || !arg.endsWith(")")) 
              reportError(Error(cmd));
         else report(cmd, eval(cmd));
@@ -35,17 +36,19 @@ function doMethod(met) {
         reportError(e);
     }
 }
-function doClick2() { //target == list2
-    let k = lineNumber(list2), obj = objP[k];
+function doClick2(c) { //event.target == list2
+    if (c == 0 && _ instanceof Array) return;
+    let obj = objP[c];
     if (obj === ITERABLE) 
-         display(objToArray(co));
+         display(objToArray(_));
     else display(obj);
 }
 function previous() {
-    let n = hist.length; if (n<2) return;
+    if (hist.length < 2) return;
     hist.pop(); display(hist.pop()); 
 }
 function remove() {
+    if (objA.length < 2) return;
     objA.splice(current, 1);
     list1.removeChild(list1.children[current]);
     previous();
@@ -56,10 +59,10 @@ function doKey() {
       case "Delete":
         remove(); return;
       case "ArrowUp":
-        event.preventDefault();
+        event.stopPropagation();
         displayItem(current-1); return;
       case "ArrowDown":
-        event.preventDefault();
+        event.stopPropagation();
         displayItem(current+1); return;
       case "ArrowLeft": case "Backspace":
         previous(); return;
@@ -67,7 +70,7 @@ function doKey() {
 }
 function doEnter() {
     if (event.key == "Enter") try {
-        event.preventDefault();
+        event.stopPropagation();
         let exp = inp.value;
         report(exp, eval(exp));
     } catch(e) {
@@ -91,13 +94,13 @@ function objToString(obj) {
     return s;
 }
 function arrayToList(a, L) {
-    const MAX = 30, OBJ = "[object ", RB = "]";
+    const MAX = 28, OBJ = "[object ", RB = "]";
     let list = "";
     for (let obj of a) {
         if (!obj) continue;
         let s = (L == list1? objToString(obj) : obj);
         s = trunc(s, MAX);
-        if (s.includes(OBJ)) {
+        if (s[0] != '"' && s.includes(OBJ)) {
           s = s.replace(OBJ, "<B>");
           if (s.includes(RB))
             s = s.replace(RB, "</B>");
@@ -118,57 +121,63 @@ function display(f) {
         out.style.backgroundColor = "cyan"; return;
     }
     let i = objA.indexOf(f);
-    if (i >= 0) {
-        displayItem(i);
-    } else {
-        objA.push(f); arrayToList(objA, list1);
-        displayItem(-1); 
-    }
-    return f;
+    if (i < 0) objA.push(f); 
+    arrayToList(objA, list1);
+    displayItem(i); return f;
 }
-function selectCurrent(dark) {
-    let c = list1.children[current]; if (!c) return;
-    c.style.color = (dark? "white" : "");
-    c.style.backgroundColor = (dark? "blue" : "");
-}
-function selectItem(c) {
+function displayItem(c) {
+  function selectCurrent(dark) {
+    let b = list1.children[current]; if (!b) return;
+    b.style.color = (dark? "white" : "");
+    b.style.backgroundColor = (dark? "blue" : "");
+    if (c == list1.length-1) b.scrollIntoView();
+  }
+  function selectItem() {
     selectCurrent(false); current = c;
     if (c < 0) current = objA.length-1;
     if (c >= objA.length) current = 0;
     selectCurrent(true); 
-}
-function addProperty(key) {
-    let obj = co[key];
+  }
+  function addProperty(key) {
+    let obj = _[key];
     let s = key +": "+ objToString(obj);
     objP.push(obj); prop.push(s);
-}
-function addMethods(metA) {
+  }
+  function addMethods(metA) {
     for (let m of metA) meth.push(m); 
-}
-function add_proto_() {
-    if (typeof co == "string") 
-addMethods(["charAt", "concat", "replace", "search", "split", "substring"]);
-    if (co instanceof Array) 
+  }
+  function add_proto_() {
+    if (typeof _ == "string") {
+        addMethods(["charAt", "concat", "indexOf", "repeat", "replace", 
+        "search", "split", "substring"]); addProperty("length");
+    } else if (_ instanceof Array) {
         addMethods(["indexOf", "join", "push", "pop", "reverse", "splice"]);
-    if (co instanceof Set) 
-        addMethods(["add", "clear", "delete", "has", "values"]);
-    if (co instanceof RegExp) { 
+        addProperty("length");
+    } else if (_ instanceof Set) {
+        addMethods(["add", "clear", "delete", "has", "values"]); 
+        addProperty("size");
+    } else if (_ instanceof Map) {
+        addMethods(["delete", "entries", "get", "has", "keys", "set"]); 
+        addProperty("size");
+    } else if (_ instanceof RegExp) { 
         addMethods(["test", "exec"]); addProperty("flags");
     }
-    /*if (co instanceof Error) { 
+    /*else if (_ instanceof Error) { 
         addProperty("fileName"); addProperty("stack");
     }*/
-}
-function displayItem(c) {
-    //co is global -- current object
-    selectItem(c); co = objA[current];
-    objP.length = 1; prop.length = 1; meth.length = 0;
-    objP[0] = ITERABLE; prop[0] = "* *"+co.constructor.name;
-    if (co[Symbol.iterator]) prop[0] += " — Iterable"
-    for (let key in co) 
+  }
+    //_ is global -- current object
+    selectItem(); _ = objA[current];
+    objP.length = 1; let prop = [], meth = [];
+    objP[0] = null; prop[0] = "* *"+_.constructor.name;
+    if (_[Symbol.iterator]) {
+        objP[0] = ITERABLE; prop[0] += " — Iterable";
+    }
+    add_proto_(); list1.focus();
+    for (let key in _) 
       try {
         if (key == "webkitStorageInfo") continue;
-        let obj = co[key];
+        let obj = _[key];
         if (typeof obj == "function") {
             meth.push(key);
         } else if (obj != null) {
@@ -178,14 +187,13 @@ function displayItem(c) {
         }
       } catch(error) { //silently ignore
       }
-    add_proto_(); list1.focus();
     arrayToList(prop, list2);
     arrayToList(meth, list3);
     let p = prop.length-1, m = meth.length;
     let s = p+" properties and "+m+" methods";
-    out.innerText = trunc(co.toString(), 25)+" — "+s; 
+    out.innerText = trunc(_.toString(), 25)+" — "+s; 
     out.style.backgroundColor = "";
     let n = hist.length;
-    if (hist[n-1] !== co) hist.push(co); 
+    if (hist[n-1] !== _) hist.push(_); 
     if (n > 50) hist.splice(0, 20);
 }
