cee4d582499cc9632be5d86d5fa05a467cc57e93
M Akif Eyler <ma.eyler@gmail.com>
2019-09-03 06:43:37 +0300

    max-width to body

--- simple/TurkceSayi.js
+++ simple/TurkceSayi.js
@@ -8,14 +8,14 @@ const ones = ["", "bir ","iki ","üç ","dört ","beş "
 function threeDigits(n) {
     let s = "";
     if (n < 1 || n > 999)
-        return "hata "+n;
+      return "hata "+n;
     if (n > 199) 
-        s += ones[Math.trunc(n/100)];
+      s += ones[Math.trunc(n/100)];
     if (n > 99) {
-        s += "yüz "; n %= 100;
+      s += "yüz "; n %= 100;
     }
     if (n > 9) {
-        s += tens[Math.trunc(n/10)]; n %= 10;
+      s += tens[Math.trunc(n/10)]; n %= 10;
     }
     return s + ones[n];
 }
@@ -24,12 +24,12 @@ function toWord(n) {
     if (n == 0) return "sıfır";
     if (n < 0) return "eksi "+toWord(-n);
     for (let i=4; i>=0; i--)
-        if (n >= num[i]) {
-            const k = Math.trunc(n/num[i]);
-            if (i==1 && n<2000) s += word[1];
-            else s += threeDigits(k)+word[i]; 
-            n %= num[i];
-        }
+      if (n >= num[i]) {
+        const k = Math.trunc(n/num[i]);
+        if (i==1 && n<2000) s += word[1];
+        else s += threeDigits(k)+word[i]; 
+        n %= num[i];
+      }
     return s;
 }
 
